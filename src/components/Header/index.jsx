4accfc5dcdca6de892d07f5bb847e1a8826deb27
Gabriel Lima <gabriellima.glgs@gmail.com>
2022-03-06 18:40:26 -0300

    delete files

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,51 @@
-import react from 'react'
+import react, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa'
 import './header.css'
 
 
 export default function Header() {
 
+    const MenuItems = [
+        {
+            title: 'Cadastrar localização',
+            url: '#',
+            cName: 'nav-links',
+        },
+        {
+            title: 'Cadastrar Tartaruga',
+            url: '#',
+            cName: 'nav-links',
+        },
+        {
+            title: 'a definir',
+            url: '#',
+            cName: 'nav-links',
+        },
+    ]
+
+    const [clicked, setClicked] = useState(false)
+
+    function handleclick(){
+        console.log(clicked)
+        setClicked(!clicked) 
+    }
+
     return (
-        <div>
-            <div className="nav">
-                <div className="navlink">
-                    <h1>Logo</h1>
-                </div>
-                <div className="bars"  >
-                    <FaBars />
-                </div>
-                <div className="nav-menu">
-                    <Link className="navlink">Cadastrar Marcador</Link>
-                    <Link className="navlink">Cadastrar Tartaruga</Link>
-                    <Link className="navlink">Cadastrar</Link>
-                </div>
-                <div className="navBtnLink">Logar</div>
+        <nav className="nav">
+            <h1 className="navbar-logo">
+                Turtle
+            </h1>
+            <div className="menu-icon" onClick={handleclick}>
+              {clicked ? <FaTimes className='nav-icon'/> : <FaBars className='nav-icon'/> } 
             </div>
-        </div>
+            <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
+                {MenuItems.map((item, index) => {
+                    return (
+                        <li key={index}><Link className={item.cName} to="">{item.title}</Link></li>
+                    )
+                })}
+            </ul>
+        </nav>
     )
 }
