0e51bb290c8ab8a6243bb5d3a30da06637882afd
Gabriel Lima <gabriellima.glgs@gmail.com>
2022-04-21 19:08:48 -0300

    register non reproduct form

---
+++ src/components/NonReproduct/index.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import firebase from 'firebase'
+import { toast } from 'react-toastify'
+
+
+export default function NonReproduct() {
+
+    var tartarugas = [
+        'Tartaruga-verde (Chelonia mydas)',
+        'Tartaruga-cabeçuda (Caretta caretta)',
+        'Tartaruga-oliva (Lepidochelys olivacea)',
+        'Tartaruga de couro (Dermochelys Coriacea)',
+        'Tartaruga-de-pente (Eretmochelys imbricata)'
+    ]
+
+
+    const [nomeMarcador, setNomeMarcador] = useState('')
+    const [marcasVisiveis, setMarcasVisiveis] = useState('')
+    const [causaProvavel, setCausaProvavel] = useState('')
+    const [latitude, setLatitude] = useState('')
+    const [longitude, setLongitude] = useState('')
+    const [especie, setEspecie] = useState('')
+
+
+    async function handleRegisterNest(e) {
+        e.preventDefault()
+
+        if (nomeMarcador === '') {
+            toast.warning('digite o nome do local')
+        }
+        else if (especie === '') {
+            toast.warning('selecione a especie')
+        } else if (causaProvavel === '') {
+            toast.warning('escreva as possiveis causas da morte')
+        } else if (marcasVisiveis === '') {
+            toast.warning('escreva sobre as marcas visivies')
+        }
+        else {
+
+            await firebase.firestore().collection('não-reprodutivo-localizações').add({
+                'nomeMarcador': nomeMarcador,
+                'latitude': latitude,
+                'longitude': longitude,
+                'especie': especie,
+                'marcasVisiveis' : marcasVisiveis,
+                'provavelCausa' : causaProvavel
+
+            }).then(() => {
+
+                toast.success('Informações cadastrada com sucesso!')
+
+                setNomeMarcador('')
+                setLatitude('')
+                setLongitude('')
+                setEspecie('')
+                setCausaProvavel('')
+                setMarcasVisiveis('')
+            }).catch((e) => {
+                console.log('========')
+                console.log(e)
+            })
+        }
+    }
+
+    return (
+        <>
+
+            <form onSubmit={handleRegisterNest}>
+                <label>Digite o nome do local</label>
+                <input type="text" className="form-input" placeholder='Digite o nome do local' value={nomeMarcador} onChange={(e) => setNomeMarcador(e.target.value)} />
+
+                <label>Marcas Visiveis</label>
+                <textarea className='form-input' cols="100" rows="100" value={marcasVisiveis} onChange={(e) => setMarcasVisiveis(e.target.value)}></textarea>
+
+                <label>Causas Provaveis</label>
+                <textarea className='form-input' cols="100" rows="100" value={causaProvavel} onChange={(e) => setCausaProvavel(e.target.value)} ></textarea>
+
+                <label>Selecione o tipo da especie</label>
+                <select className='form-input' onChange={(e) => setEspecie(e.target.value)}>
+                    <option selected>Escolha uma especie</option>
+                    {tartarugas.map((item) => {
+                        return (
+                            <option>{item}</option>
+                        );
+                    })}
+                </select>
+
+                <button className='form-button' type='submit'>Salvar</button>
+            </form>
+        </>
+
+    )
+
+}
--- src/pages/NestLocations/index.jsx
+++ src/pages/NestLocations/index.jsx
@@ -1,143 +1,39 @@
+/* eslint-disable no-const-assign */
 import React, { useState } from 'react'
 import Header from '../../components/Header'
 import './nestLocations.css'
-import firebase from 'firebase'
-import { toast } from 'react-toastify'
-
+import NestForm from '../../components/NestForm';
+import NonReproduct from '../../components/NonReproduct';
 
 export default function NestLocations() {
 
-
-    var tartarugas = [
-        'Tartaruga-verde (Chelonia mydas)',
-        'Tartaruga-cabeçuda (Caretta caretta)',
-        'Tartaruga-oliva (Lepidochelys olivacea)',
-        'Tartaruga de couro (Dermochelys Coriacea)',
-        'Tartaruga-de-pente (Eretmochelys imbricata)'
-    ]
-
-    
-    const [nomeMarcador, setNomeMarcador] = useState('')
-    const [latitude, setLatitude] = useState('')
-    const [longitude, setLongitude] = useState('')
-
-    const [especie, setEspecie] = useState('')
-    const [qtdOvosEclodidos, setQtdOvosEclodidos] = useState('')
-    const [qtdOvosNEclodidos, setQtdOvosNEclodidos] = useState('')
-    const [natimorto, setNatimorto] = useState('')
-    const [dataEclosao, setDataEclosao] = useState('')
-    const [localizacao, setLocalizacao] = useState('')
-
-
-    async function handleRegisterNest(e) {
-        e.preventDefault()
-
-        if (nomeMarcador === ''){
-            toast.warning('digite o nome do local')
-        }
-        else if (especie === ''){
-            toast.warning('selecione a especie')
-        }
-        else if (qtdOvosEclodidos === ''){
-            toast.warning('digite a quantidade de ovos eclodidos')
-        }
-        else if (qtdOvosNEclodidos === ''){
-            toast.warning('digite a quantidade de ovos não eclodidos')
-        }
-        else if (natimorto === ''){
-            toast.warning('digite a quantidade de natimortos')
-        }
-        else if (dataEclosao === ''){
-            toast.warning('digite a data de eclosão')
-        }
-        else if (localizacao === ''){
-            toast.warning('selecione o tipo de localização')
-        } else {
-            
-            await firebase.firestore().collection('ninhos-localizações').add({
-                'nomeMarcador' : nomeMarcador,
-                'latitude' : latitude,
-                'longitude' : longitude,
-                'especie' : especie,
-                'qtdOvosEclodidos' : qtdOvosEclodidos,
-                'qtdOvosNEclodidos' : qtdOvosNEclodidos,
-                'natimorto' : natimorto,
-                'dataEclosão' : dataEclosao,
-                'localizacao' : localizacao
-            }).then(() => {
-    
-                toast.success('Ninho cadastrado com sucesso!')
-                
-                setNomeMarcador('')
-                setLatitude('')
-                setLongitude('')
-                setEspecie('')
-                setQtdOvosEclodidos('')
-                setQtdOvosNEclodidos('')
-                setNatimorto('')
-                setDataEclosao('')
-                setLocalizacao('')
-            }).catch((e) => {
-                console.log('========')
-                console.log(e)
-            })
-        }
-    }
+    const [option, setOption] = useState('')
 
     return (
         <>
-            <Header/>
+            <Header />
             <div className="container">
                 <div className="container-form">
-                    <h2>Cadastrar Ninho</h2>
-                    <hr />
-                    <form onSubmit={handleRegisterNest}>
-                        <label>Digite o nome do local</label>
-                        <input type="text" className="form-input" placeholder='Digite o nome do local' value={nomeMarcador} onChange={(e) => setNomeMarcador(e.target.value)} />
-                        
-                        <label>Selecione o tipo da especie</label>
-                        <select className='form-input' onChange={(e) => setEspecie(e.target.value)}> 
-                        <option selected>Escolha uma especie</option>
-                            {tartarugas.map((item) => {
-                                return(
-                                    <option>{item}</option>
-                                );
-                            })}
-                        </select>
 
-                        <div className='label-by-qtd'>
-                            <div className='format-label'>
-                                <label>Quantidade de ovos Eclodidos</label>
-                                <input type="number" className="form-input" placeholder='Quantidade de ovos Eclodidos' value={qtdOvosEclodidos} onChange={(e) => setQtdOvosEclodidos(e.target.value)} />
-                            </div>
-                            
-                            <div className='format-label'>
-                                <label>Quantidade de ovos não Eclodidos</label>
-                                <input type="number" className="form-input" placeholder='Quantidade de ovos não eclodidos' value={qtdOvosNEclodidos} onChange={(e) => setQtdOvosNEclodidos(e.target.value)}/>
-                            </div>
-                            
-                            <div className='format-label'>
-                                <label>Quantidade de Natimortos</label>
-                                <input type="number" className="form-input" placeholder='Quantidade de natimortos' value={natimorto} onChange={(e) => setNatimorto(e.target.value)}/>
-                            </div>
+                    <h2>{option === 'nReprodutivos' ? 'Cadastro Não Reprodutivo' : 'Cadastrar Ninhos' }</h2>
+                    <hr />
+
+                    <div className='options'>
+                        <div className='radio-label'>
+                            <input type="radio" id='ninhos' name="ninhos" value='ninhos' onChange={(e) => setOption(e.target.value)} style={{ marginRight: '10px' }} defaultChecked/>
+                            <label style={{ marginRight: '10px' }}>Ninhos de Tartarugas</label>
                         </div>
 
-                        <label>Data da Eclosão</label>
-                        <input type="datetime-local" className="form-input" placeholder='Data de Eclosão' value={dataEclosao} onChange={(e) => setDataEclosao(e.target.value)}/>
-
-                        <label>Selecione a localização</label>
-                        <select className='form-input' onChange={(e) => setLocalizacao(e.target.value)}>
-                            <option selected>Escolha um local</option>
-                            <option>In Situ</option>
-                            <option>Translocado</option>
-                        </select>
-
-                        <button className='form-button' type='submit'>Cadastrar Ninho</button>
-                    </form>
+                        <div className='radio-label'>
+                            <input type="radio" name='ninhos' value='nReprodutivos' onChange={(e) => setOption(e.target.value)} style={{ marginRight: '10px' }} />
+                            <label >Não Reprodutivos</label>
+                        </div>
+                    </div>
+                   {option === 'nReprodutivos' ? <NonReproduct/> : <NestForm />}
                 </div>
             </div>
         </>
-         
+
     )
 
 }
